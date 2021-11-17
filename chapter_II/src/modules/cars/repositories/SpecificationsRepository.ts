aff01bf54eee03e2befcfd709200b35e193f72f1
Alex <alex.jrmts@gmail.com>
2021-11-17 12:23:52 -0300

    adding specifications to routes and services

---
+++ chapter_II/src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,33 @@
+import { Specification } from "../model/specification";
+import { ISpecificationRepository, ICreateSpecificationDTO } from "./ISpecificationRepository";
+
+class SpecificationRepository implements ISpecificationRepository {
+    private specifications: Specification[];
+
+    constructor() {
+        this.specifications = [];
+    }
+
+    create({ name, description }: ICreateSpecificationDTO) {
+        const specification = new Specification();
+
+        Object.assign(specification, {
+            name,
+            description,
+            created_at: new Date()
+        });
+
+        this.specifications.push(specification);
+    }
+
+    list(): Specification[] {
+        return this.specifications;
+    }
+
+    findByName(name: string): Specification {
+        const specification = this.specifications.find((specification) => specification.name === name);
+        return specification;
+    }
+}
+
+export { SpecificationRepository };
